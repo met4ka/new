fd2ce6203b4af29c76d91773ec684910dbef288f
Win10Pro <met4ka@gmail.com>
2022-10-18 23:05:26 +0400

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.util.SimpleTimeZone;
+
+public class main {
+    public static void main(String[] args) {
+        design app = new design();
+        app.setVisible(true);
+    }
+
+}
